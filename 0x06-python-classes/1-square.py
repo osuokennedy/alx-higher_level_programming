21f8b513226c57e7181c28428f13f0c0f33a2a50
osuokennedy <osuokennedy@gmail.com>
2023-03-30 15:48:31 +0300

    Write a class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
-"""Square Module."""
+ #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
     """Defines a square."""
+
     def __init__(self, size):
-        """ Constructor
+        """Constructor.
         Args:
             size: length of side of the square.
         """
         self.__size = size
+
         
